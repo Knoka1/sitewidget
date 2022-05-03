e6b24ccba95b29727929e1fdacc7cc645421063d
Alec do Couto <alec.couto@transfero.com>
2022-05-03 18:01:45 -0300

    added FeedbackSuccesStep page and functionalities

--- src/components/WidgetForm/index.tsx
+++ src/components/WidgetForm/index.tsx
@@ -8,6 +8,7 @@ import thoughtImageUrl from "../../resources/imgs/thought.svg";
 
 import FeedbackTypeStep from "./Steps/FeedbackTypeStep";
 import FeedbackContentStep from "./Steps/FeedbackContentStep";
+import FeedbackSuccessStep from "./Steps/FeedbackSuccessStep";
 
 interface WidgetFormProps {
   company: string;
@@ -31,19 +32,30 @@ export type FeedbackType = keyof typeof feedbackTypes;
 
 export function WidgetForm(props: WidgetFormProps) {
   const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null);
+  const [feedbackSent, setFeedbackSent] = useState(false);
+
   function handleRestartWidget() {
+    setFeedbackSent(false);
+
     setFeedbackType(null);
   }
 
   return (
     <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
-      {!feedbackType ? (
-        <FeedbackTypeStep onFeedBackTypeHandler={setFeedbackType} />
+      {feedbackSent ? (
+        <FeedbackSuccessStep onWidgetRestart={handleRestartWidget} />
       ) : (
-        <FeedbackContentStep
-          feedbackType={feedbackType}
-          onWidgetRestart={handleRestartWidget}
-        />
+        <>
+          {!feedbackType ? (
+            <FeedbackTypeStep onFeedBackTypeHandler={setFeedbackType} />
+          ) : (
+            <FeedbackContentStep
+              feedbackType={feedbackType}
+              onWidgetRestart={handleRestartWidget}
+              onFeedbackSent={() => setFeedbackSent(true)}
+            />
+          )}
+        </>
       )}
       <footer className="text-xs text-neutral-400">
         <a className="underline underline-offset-2" href={props.link}>
